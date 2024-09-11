51100057b49d91167c72e6a14dca053c688130ed
eastdrop <lyokha7777@gmail.com>
2024-09-11 19:19:49 +0300

    added supportFragmentManager to transaction Fragment

--- app/src/main/java/com/example/sem_11_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/sem_11_mvvm/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.sem_11_mvvm.ui.main.MainFragment
+import com.example.sem_11_mvvm.ui.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.commit {
+            replace<MainFragment>(R.id.main)
+            setReorderingAllowed(true)
+            addToBackStack("name") // Name can be null
+        }
         enableEdgeToEdge()
         setContentView(R.layout.fragment_main_fragment)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -17,6 +25,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
 
     }
 }
